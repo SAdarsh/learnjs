59eeb3cd078f3fc9a2381cae0a57ed4c2710ab8c
Adarsh <surakaramadarsh@gmail.com>
2022-04-10 12:21:07 +0200

    values and variables 🚀

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -1,8 +1,11 @@
 console.log("Adarsh");
 console.log(28);
+console.log("Stuttgart");
 
-let firstName = "Adarsh"
+let firstName = "Adarsh";
 let age = 28;
+let currentPlace = "Stuttgart";
 
-console.log(firstName)
-console.log(age)
+console.log(firstName);
+console.log(age);
+console.log(currentPlace);
