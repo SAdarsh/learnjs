82ab96b11caca1c1ed600253f6e0326bb167ec0e
Adarsh <surakaramadarsh@gmail.com>
2022-04-10 15:38:53 +0200

    data types 🚀

---
+++ 01-Fundamentals-Part-3/script.js
@@ -0,0 +1,19 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun); // boolean
+console.log(typeof true); // boolean
+console.log(typeof 23); // integer
+console.log(typeof "Adarsh"); // string
+
+javascriptIsFun = 'YES!'; // JavaScript is dynamicaa=lly typed
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year); // undefined
+console.log(typeof year); // undefined
+
+year = 1991; // defining year
+console.log(typeof year);
+
+console.log(typeof null); // bug - type is not object
