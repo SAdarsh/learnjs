1356553f6a636aa870fe8acb94cf00cd77f73ddf
Adarsh <surakaramadarsh@gmail.com>
2022-04-10 16:18:30 +0200

    operators 🚀

---
+++ 01-Fundamentals-Part-5/script.js
@@ -0,0 +1,20 @@
+const now = 2037; // assignment
+const ageJonas = now - 1991; // substraction
+const ageSarah = now - 2018; // substraction
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); // multiplication, division, and powerof
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName); // concatenation
+
+let x = 10 + 5; // addition
+x += 10 // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 4
+x--; // x = x - 4
+console.log(x)
+
+console.log(ageJonas > ageSarah); // comparision operator; >, <, <=, >=
+console.log(ageSarah >= 18); 
